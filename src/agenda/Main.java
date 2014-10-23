e7544aa6d534da90a242940e6dcd359c73a2b332
Eduardo Castro <educcjr@gmail.com>
2014-10-23 13:30:41 -0200

    Ajustes na conexão MySQL

--- src/agenda/Main.java
+++ src/agenda/Main.java
@@ -1,13 +1,14 @@
 package agenda;
 
-public class Main 
+import conexao.ConexaoMySQL;
+
+public class Main
 {
     public static void main(String args[])
     {
-      JanelaPrincipal jp = new JanelaPrincipal();
-      jp.setVisible(true);
-      
-      Conexao conn = new Conexao();
-      conn.conecta();
+        ConexaoMySQL.conecta();
+        JanelaPrincipal jp = new JanelaPrincipal();
+        jp.setVisible(true);
+        ConexaoMySQL.encerra();
     }
 }
---
+++ src/conexao/ConexaoMySQL.java
@@ -0,0 +1,52 @@
+package conexao;
+
+import java.sql.*;
+
+public class ConexaoMySQL
+{
+    private static Connection connection = null;
+    
+    public ConexaoMySQL()
+    {
+        
+    }
+    
+    public static Connection conecta()
+    {  
+    	try 
+    	{
+            // Configurações na conexão MySQL
+            String url = "jdbc:mysql://localhost/agenda";
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName);
+            connection = DriverManager.getConnection(url,"root","" );
+
+            System.out.println("Conectado com Sucesso!");
+            return connection;
+        } 
+    	catch (SQLException sqle)
+    	{
+            System.out.println("Não foi possivel conectar no banco."+ sqle);
+            return null;
+        }
+        catch(ClassNotFoundException cnfe)
+        {
+            System.out.println("Driver não encontrado."+ cnfe);
+            return null;
+        }
+    }
+    
+    public static boolean encerra()
+    {
+    	try 
+    	{
+            connection.close();
+            System.out.println("Conexão Encerrada.");
+            return true;
+        } 
+    	catch (SQLException sqle) 
+    	{
+            return false;
+        }
+    }
+}
